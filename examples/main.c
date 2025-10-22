8c89c1ad5af08292d9d4c9e98ceb0a3b5c0606d1
emir <postpost42@yandex.ru>
2025-10-22 11:08:01 +0300

    fix names

--- examples/main.c
+++ examples/main.c
@@ -16,11 +16,11 @@ void handle_event_type_a_and_b(const es_event_t *event, es_event_bus_t *bus, voi
     UNUSED(ctx);
 
     switch (es_get_event_type(event)) {
-        case EV_TYPE_A:
+        case ES_EV_TYPE_A:
             printf("A\n");
-            es_publish(bus, EV_TYPE_C);
+            es_publish(bus, ES_EV_TYPE_C);
             break;
-        case EV_TYPE_B: {
+        case ES_EV_TYPE_B: {
             ES_EV_EXPECT(event, int);
             const int x = ES_EV_VAL(event, int);
             printf("B %d\n", x);
@@ -36,16 +36,16 @@ void handle_event_type_c_and_d(const es_event_t *event, es_event_bus_t *bus, voi
     assert(bus);
 
     switch (es_get_event_type(event)) {
-        case EV_TYPE_C: {
+        case ES_EV_TYPE_C: {
             ES_CTX_EXPECT(ctx, user_ctx_t);
             const user_ctx_t user_ctx = ES_CTX_VAL(ctx, user_ctx_t);
             printf("C %d\n", user_ctx.x);
             break;
         }
-        case EV_TYPE_D: {
+        case ES_EV_TYPE_D: {
             printf("D\n");
             const int x = 5;
-            ES_PUBLISH_LDATA(bus, EV_TYPE_B, x);
+            ES_PUBLISH_LDATA(bus, ES_EV_TYPE_B, x);
             // or
             // ES_PUBLISH_RDATA(bus, EV_TYPE_B, int, 5);
             break;
@@ -57,10 +57,10 @@ void handle_event_type_c_and_d(const es_event_t *event, es_event_bus_t *bus, voi
 
 void register_events(es_event_bus_t *bus) {
     static user_ctx_t ctx = {.x = 10};
-    es_subscribe(bus, EV_TYPE_A, handle_event_type_a_and_b, NULL);
-    es_subscribe(bus, EV_TYPE_B, handle_event_type_a_and_b, NULL);
-    es_subscribe(bus, EV_TYPE_C, handle_event_type_c_and_d, &ctx);
-    es_subscribe(bus, EV_TYPE_D, handle_event_type_c_and_d, NULL);
+    es_subscribe(bus, ES_EV_TYPE_A, handle_event_type_a_and_b, NULL);
+    es_subscribe(bus, ES_EV_TYPE_B, handle_event_type_a_and_b, NULL);
+    es_subscribe(bus, ES_EV_TYPE_C, handle_event_type_c_and_d, &ctx);
+    es_subscribe(bus, ES_EV_TYPE_D, handle_event_type_c_and_d, NULL);
 }
 
 int main(void) {
@@ -69,9 +69,9 @@ int main(void) {
 
     register_events(bus);
 
-    es_publish(bus, EV_TYPE_A);
-    es_publish(bus, EV_TYPE_C);
-    es_publish(bus, EV_TYPE_D);
+    es_publish(bus, ES_EV_TYPE_A);
+    es_publish(bus, ES_EV_TYPE_C);
+    es_publish(bus, ES_EV_TYPE_D);
 
     es_bus_destroy(bus);
 }
